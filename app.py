5a7fc4f30a7bb4fa80be994f0e66ea78caf98555
Rushikesh Nagarkar <138763048+rushikeshnagarkar@users.noreply.github.com>
2025-06-10 18:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -368,7 +368,7 @@ def send_payment_message(frm, name, address, pincode, items, order_amount, refer
             },
             "reminder_enable": True,
             "reference_id": reference_id,
-            "callback_url": "https://13.202.207.66:5000/payment-callback",
+            "callback_url": "http://13.202.207.66:5000/payment-callback",
             "callback_method": "get"
         }
         print(f"Razorpay payment link data: {payment_link_data}")
